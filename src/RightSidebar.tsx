8c993431748b4cb46f6d2d7090347d5aeae8dc09
Vivek Dasari <vivdasari@gmail.com>
2019-09-16 09:06:21 -0700

    Draft.js editor

--- src/RightSidebar.tsx
+++ src/RightSidebar.tsx
@@ -1,13 +1,36 @@
 import React, { useRef } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import {
+  Editor,
+  EditorState,
+  ContentBlock,
+  ContentState,
+  DraftBlockType
+} from 'draft-js';
 import 'draft-js/dist/Draft.css';
+import './RightSidebar.css';
+
+// const EditorLine: React.FC<DraftBlockType>
 
 const RightSidebar: React.FC = () => {
   const [editorState, setEditorState] = React.useState(
-    EditorState.createEmpty()
+    EditorState.moveFocusToEnd(
+      EditorState.createWithContent(
+        ContentState.createFromText('## Write Code; Draw Blocks\n\n')
+      )
+    )
   );
 
   const editorRef = useRef<Editor>(null);
+
+  function myBlockStyleFn(contentBlock: ContentBlock): string {
+    const type = contentBlock.getType();
+    switch (type) {
+      case 'unstyled':
+        return 'unstyled-code-text';
+      default:
+        return 'unstyled-code-text';
+    }
+  }
 
   function focusEditor() {
     if (editorRef && editorRef.current) {
@@ -22,13 +45,13 @@ const RightSidebar: React.FC = () => {
   return (
     <div
       className="flex-elem"
-      style={{ cursor: 'text' }}
+      style={{ cursor: 'text', background: 'var(--odys-code-background)' }}
       onClick={() => focusEditor()}
     >
       <Editor
         editorState={editorState}
         onChange={setEditorState}
-        placeholder="Enter some text..."
+        blockStyleFn={myBlockStyleFn}
         ref={editorRef}
       />
     </div>
