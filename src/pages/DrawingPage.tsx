32f47b94408d7ccb255c13b10c78986863e9e82e
Vivek Dasari <vivdasari@gmail.com>
2020-02-26 14:46:12 -0800

    remove editor component from page

--- src/pages/DrawingPage.tsx
+++ src/pages/DrawingPage.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import DrawingBoard from '../components/DrawingBoard';
-import Sidebar from '../components/Sidebar';
-import OdysEditor from '../components/OdysEditor';
 
 const DrawingPage: React.FC = () => {
   return (
@@ -12,11 +10,6 @@ const DrawingPage: React.FC = () => {
       }}
       className="row"
     >
-      <div className="col flex-elem">
-        <Sidebar>
-          <OdysEditor></OdysEditor>
-        </Sidebar>
-      </div>
       <div className="col flex-elem">
         <DrawingBoard></DrawingBoard>
       </div>
