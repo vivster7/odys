edb7b5d67e54cd19a62cfa61b189c12c027dae13
Vivek Dasari <vivdasari@gmail.com>
2020-03-22 10:40:09 -0700

    init reducer refactor - drag and pan

--- src/globals.ts
+++ src/globals.ts
@@ -2,6 +2,16 @@ import React, { Dispatch } from 'react';
 import Shape from './shapes/Shape';
 import { v4 } from 'uuid';
 import { RectProps, RECT_HEIGHT, RECT_WIDTH } from './shapes/Rect';
+import dragReducerMap, {
+  OdysStartDragAction,
+  OdysDragAction,
+  OdysEndDragAction
+} from './reducers/drag';
+import panReducerMap, {
+  OdysStartPanAction,
+  OdysPanAction,
+  OdysEndPanAction
+} from './reducers/pan';
 
 export type NEAnchor = 'NEAnchor';
 export type NWAnchor = 'NWAnchor';
@@ -70,39 +80,6 @@ export interface OdysAddShapeAction extends GlobalActionType {
 export interface OdysDeleteShapeAction extends GlobalActionType {
   type: 'ODYS_DELETE_SHAPE_ACTION';
   id: string;
-}
-
-export interface OdysStartDragAction extends GlobalActionType {
-  type: 'ODYS_START_DRAG_ACTION';
-  id: string;
-  clickX: number;
-  clickY: number;
-}
-
-export interface OdysDragAction extends GlobalActionType {
-  type: 'ODYS_DRAG_ACTION';
-  clickX: number;
-  clickY: number;
-}
-
-export interface OdysEndDragAction extends GlobalActionType {
-  type: 'ODYS_END_DRAG_ACTION';
-}
-
-export interface OdysStartPanAction extends GlobalActionType {
-  type: 'ODYS_START_PAN_ACTION';
-  clickX: number;
-  clickY: number;
-}
-
-export interface OdysPanAction extends GlobalActionType {
-  type: 'ODYS_PAN_ACTION';
-  clickX: number;
-  clickY: number;
-}
-
-export interface OdysEndPanAction extends GlobalActionType {
-  type: 'ODYS_END_PAN_ACTION';
 }
 
 export interface OdysStartNewRectByClickAction extends GlobalActionType {
@@ -187,53 +164,29 @@ export function globalStateReducer(
 ): GlobalState {
   console.log(action.type);
   const m = {
-    ODYS_RAISE_SHAPE_ACTION: () =>
-      onOdysRaiseShape(state, action as OdysRaiseShapeAction),
-    ODYS_ADD_SHAPE_ACTION: () =>
-      onOdysAddShapeAction(state, action as OdysAddShapeAction),
-    ODYS_DELETE_SHAPE_ACTION: () =>
-      onOdysDeleteShapeAction(state, action as OdysDeleteShapeAction),
-    ODYS_START_DRAG_ACTION: () =>
-      onOdysStartDragAction(state, action as OdysStartDragAction),
-    ODYS_START_PAN_ACTION: () =>
-      onOdysStartPanAction(state, action as OdysStartPanAction),
-    ODYS_START_RESIZE_SHAPE_ACTION: () =>
-      onOdysStartResizeShapeAction(state, action as OdysStartResizeShapeAction),
-    ODYS_SELECT_ACTION: () =>
-      onOdysSelectAction(state, action as OdysSelectAction),
-    ODYS_START_NEW_RECT_BY_CLICK_ACTION: () =>
-      onOdysStartNewRectByClickAction(
-        state,
-        action as OdysStartNewRectByClickAction
-      ),
-    ODYS_SELECTED_SHAPE_INPUT_CHANGE_ACTION: () =>
-      onOdysSelectedShapeInputChangeAction(
-        state,
-        action as OdysSelectedShapeInputChangeAction
-      ),
-    ODYS_END_NEW_RECT_BY_CLICK_ACTION: () =>
-      onOdysEndNewRectByClickAction(
-        state,
-        action as OdysEndNewRectByClickAction
-      ),
-    ODYS_DRAG_ACTION: () => onOdysDragAction(state, action as OdysDragAction),
-    ODYS_END_DRAG_ACTION: () =>
-      onOdysEndDragAction(state, action as OdysEndDragAction),
-    ODYS_PAN_ACTION: () => onOdysPanAction(state, action as OdysPanAction),
-    ODYS_END_PAN_ACTION: () =>
-      onOdysEndPanAction(state, action as OdysEndPanAction),
-    ODYS_RESIZE_SHAPE_ACTION: () =>
-      onOdysResizeShapeAction(state, action as OdysResizeShapeAction),
-    ODYS_END_RESIZE_SHAPE_ACTION: () =>
-      onOdysEndResizeShapeAction(state, action as OdysEndResizeShapeAction),
-    ODYS_WHEEL_ACTION: () => onOdysWheelAction(state, action as OdysWheelAction)
+    ODYS_RAISE_SHAPE_ACTION: onOdysRaiseShape,
+    ODYS_ADD_SHAPE_ACTION: onOdysAddShapeAction,
+    ODYS_DELETE_SHAPE_ACTION: onOdysDeleteShapeAction,
+    ODYS_SELECT_ACTION: onOdysSelectAction,
+    ODYS_START_NEW_RECT_BY_CLICK_ACTION: onOdysStartNewRectByClickAction,
+    ODYS_SELECTED_SHAPE_INPUT_CHANGE_ACTION: onOdysSelectedShapeInputChangeAction,
+    ODYS_END_NEW_RECT_BY_CLICK_ACTION: onOdysEndNewRectByClickAction,
+
+    ODYS_START_RESIZE_SHAPE_ACTION: onOdysStartResizeShapeAction,
+    ODYS_RESIZE_SHAPE_ACTION: onOdysResizeShapeAction,
+    ODYS_END_RESIZE_SHAPE_ACTION: onOdysEndResizeShapeAction,
+
+    ODYS_WHEEL_ACTION: () =>
+      onOdysWheelAction(state, action as OdysWheelAction),
+    ...dragReducerMap,
+    ...panReducerMap
   };
   const fn = m[action.type];
   if (!fn) {
     throw new Error(`Unknown action ${action}`);
   }
 
-  return fn();
+  return fn(state, action as any);
 }
 
 function onOdysAddShapeAction(
@@ -253,33 +206,6 @@ function onOdysDeleteShapeAction(
   return {
     ...state,
     shapes: state.shapes.filter(s => s.id !== action.id)
-  };
-}
-
-function onOdysStartDragAction(
-  state: GlobalState,
-  action: OdysStartDragAction
-): GlobalState {
-  return {
-    ...state,
-    drag: {
-      id: action.id,
-      clickX: action.clickX,
-      clickY: action.clickY
-    }
-  };
-}
-
-function onOdysStartPanAction(
-  state: GlobalState,
-  action: OdysStartPanAction
-): GlobalState {
-  return {
-    ...state,
-    pan: {
-      clickX: action.clickX,
-      clickY: action.clickY
-    }
   };
 }
 
@@ -396,94 +322,6 @@ function onOdysEndNewRectByClickAction(
   };
 }
 
-function onOdysEndDragAction(
-  state: GlobalState,
-  action: OdysEndDragAction
-): GlobalState {
-  if (state.drag) {
-    const { shapes } = state;
-    const { id } = state.drag;
-    const idx = shapes.findIndex(s => s.id === id);
-    if (idx === -1) {
-      throw new Error(`[drag] Cannot find ${id} in shapes`);
-    }
-
-    const shape = {
-      ...shapes[idx],
-      x: (shapes[idx].x as number) + shapes[idx].translateX,
-      y: (shapes[idx].y as number) + shapes[idx].translateY,
-      translateX: 0,
-      translateY: 0
-    };
-
-    return {
-      ...state,
-      drag: null,
-      newRectByClick: null,
-      pan: null,
-      shapes: [...shapes.slice(0, idx), ...shapes.slice(idx + 1), ...[shape]]
-    };
-  }
-
-  throw new Error(
-    'Could not end drag action. Was it started with ODYS_START_DRAG_ACTION?'
-  );
-}
-
-function onOdysEndPanAction(
-  state: GlobalState,
-  action: OdysEndPanAction
-): GlobalState {
-  if (state.pan) {
-    return {
-      ...state,
-      drag: null,
-      newRectByClick: null,
-      pan: null,
-      svg: {
-        ...state.svg,
-        topLeftX: state.svg.topLeftX + state.svg.translateX,
-        topLeftY: state.svg.topLeftY + state.svg.translateY,
-        translateX: 0,
-        translateY: 0
-      }
-    };
-  }
-
-  throw new Error(
-    'Could not end pan action. Was it started with ODYS_START_PAN_ACTION?'
-  );
-}
-
-function onOdysDragAction(
-  state: GlobalState,
-  action: OdysDragAction
-): GlobalState {
-  if (state.drag) {
-    const { id } = state.drag;
-    const { shapes } = state;
-    const idx = shapes.findIndex(d => d.id === id);
-    if (idx === -1) {
-      throw new Error(`[drag] Cannot find ${id} in shapes context`);
-    }
-
-    const shape = {
-      ...shapes[idx],
-      translateX: (action.clickX - state.drag.clickX) / state.svg.scale,
-      translateY: (action.clickY - state.drag.clickY) / state.svg.scale
-    };
-
-    return {
-      ...state,
-      shapes: [...shapes.slice(0, idx), ...shapes.slice(idx + 1), ...[shape]]
-    };
-  }
-
-  throw new Error(
-    'Cannot ODYS_DRAG_ACTION without `state.drag` (did ODYS_START_DRAG_ACTION fire first?)'
-  );
-}
-
 function onOdysResizeShapeAction(
   state: GlobalState,
   action: OdysResizeShapeAction
@@ -548,26 +386,6 @@ function onOdysEndResizeShapeAction(
 
   throw new Error(
     'Could not end resize shape action. Was it started with ODYS_START_RESIZE_SHAPE_ACTION?'
-  );
-}
-
-function onOdysPanAction(
-  state: GlobalState,
-  action: OdysPanAction
-): GlobalState {
-  if (state.pan) {
-    return {
-      ...state,
-      svg: {
-        ...state.svg,
-        translateX: action.clickX - state.pan.clickX,
-        translateY: action.clickY - state.pan.clickY
-      }
-    };
-  }
-
-  throw new Error(
-    'Cannot ODYS_PAN_ACTION without `state.pan` (did ODYS_START_PAN_ACTION fire first?)'
   );
 }
 
---
+++ src/reducers/drag.ts
@@ -0,0 +1,103 @@
+import { GlobalActionType, GlobalState } from '../globals';
+
+export interface OdysStartDragAction extends GlobalActionType {
+  type: 'ODYS_START_DRAG_ACTION';
+  id: string;
+  clickX: number;
+  clickY: number;
+}
+
+export interface OdysDragAction extends GlobalActionType {
+  type: 'ODYS_DRAG_ACTION';
+  clickX: number;
+  clickY: number;
+}
+
+export interface OdysEndDragAction extends GlobalActionType {
+  type: 'ODYS_END_DRAG_ACTION';
+}
+
+const dragReducerMap = {
+  ODYS_START_DRAG_ACTION: onOdysStartDragAction,
+  ODYS_DRAG_ACTION: onOdysDragAction,
+  ODYS_END_DRAG_ACTION: onOdysEndDragAction
+};
+
+export default dragReducerMap;
+
+function onOdysStartDragAction(
+  state: GlobalState,
+  action: OdysStartDragAction
+): GlobalState {
+  return {
+    ...state,
+    drag: {
+      id: action.id,
+      clickX: action.clickX,
+      clickY: action.clickY
+    }
+  };
+}
+
+function onOdysDragAction(
+  state: GlobalState,
+  action: OdysDragAction
+): GlobalState {
+  if (state.drag) {
+    const { id } = state.drag;
+    const { shapes } = state;
+    const idx = shapes.findIndex(d => d.id === id);
+    if (idx === -1) {
+      throw new Error(`[drag] Cannot find ${id} in shapes context`);
+    }
+
+    const shape = {
+      ...shapes[idx],
+      translateX: (action.clickX - state.drag.clickX) / state.svg.scale,
+      translateY: (action.clickY - state.drag.clickY) / state.svg.scale
+    };
+
+    return {
+      ...state,
+      shapes: [...shapes.slice(0, idx), ...shapes.slice(idx + 1), ...[shape]]
+    };
+  }
+
+  throw new Error(
+    'Cannot ODYS_DRAG_ACTION without `state.drag` (did ODYS_START_DRAG_ACTION fire first?)'
+  );
+}
+
+function onOdysEndDragAction(
+  state: GlobalState,
+  action: OdysEndDragAction
+): GlobalState {
+  if (state.drag) {
+    const { shapes } = state;
+    const { id } = state.drag;
+    const idx = shapes.findIndex(s => s.id === id);
+    if (idx === -1) {
+      throw new Error(`[drag] Cannot find ${id} in shapes`);
+    }
+
+    const shape = {
+      ...shapes[idx],
+      x: (shapes[idx].x as number) + shapes[idx].translateX,
+      y: (shapes[idx].y as number) + shapes[idx].translateY,
+      translateX: 0,
+      translateY: 0
+    };
+
+    return {
+      ...state,
+      drag: null,
+      newRectByClick: null,
+      pan: null,
+      shapes: [...shapes.slice(0, idx), ...shapes.slice(idx + 1), ...[shape]]
+    };
+  }
+
+  throw new Error(
+    'Could not end drag action. Was it started with ODYS_START_DRAG_ACTION?'
+  );
+}
