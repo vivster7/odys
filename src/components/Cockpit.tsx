8071242b336d046379b2c380b5894c3a9560c37f
Vivek Dasari <vivdasari@gmail.com>
2020-04-03 12:42:30 -0700

    init cockpit with x,y coords

---
+++ src/components/Cockpit.tsx
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react';
+import { GlobalStateContext } from '../globals';
+
+const Cockpit: React.FC = props => {
+  const { globalState } = useContext(GlobalStateContext);
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexFlow: 'row nowrap',
+        borderRadius: '8px',
+        boxShadow: '0px 4px 2px -2px grey',
+        border: '1px lightgray solid',
+        padding: '12px',
+        minWidth: '100px',
+        maxWidth: '200px',
+        justifyContent: 'space-around'
+      }}
+    >
+      <p
+        style={{
+          borderTopLeftRadius: '8px',
+          borderBottomLeftRadius: '8px',
+          backgroundColor: 'white'
+        }}
+      >
+        {globalState.svg.topLeftX.toFixed(2)}
+      </p>
+      <p
+        style={{
+          borderLeft: '1px rgba(204, 204, 204, 0.5) solid',
+          margin: '-3px 5px'
+        }}
+      ></p>
+      <p
+        style={{
+          borderTopRightRadius: '8px',
+          borderBottomRightRadius: '8px',
+          backgroundColor: 'white'
+        }}
+      >
+        {globalState.svg.topLeftY.toFixed(2)}
+      </p>
+    </div>
+  );
+};
+
+export default Cockpit;
--- src/pages/DrawingPage.tsx
+++ src/pages/DrawingPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import DrawingBoard from '../components/DrawingBoard';
+import Cockpit from '../components/Cockpit';
 
 const DrawingPage: React.FC = () => {
   return (
@@ -12,6 +13,15 @@ const DrawingPage: React.FC = () => {
     >
       <div className="col flex-elem">
         <DrawingBoard></DrawingBoard>
+      </div>
+      <div
+        style={{
+          position: 'absolute',
+          bottom: '10px',
+          right: '10px'
+        }}
+      >
+        <Cockpit></Cockpit>
       </div>
     </div>
   );
