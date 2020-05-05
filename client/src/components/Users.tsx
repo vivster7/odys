8fa52b5c8aca65c8aec6336935d588fecde12693
Vivek Dasari <vivdasari@gmail.com>
2020-05-05 15:58:09 -0700

    type Users.tsx

--- client/src/components/Users.tsx
+++ client/src/components/Users.tsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { useGetUsersQuery, User } from '../generated/graphql';
 
 const Users: React.FC = () => {
-  const [users, setUsers] = useState();
+  const [users, setUsers] = useState<User[]>([]);
   const { data, error, loading } = useGetUsersQuery();
 
   useEffect(() => {
     if (data) {
       if (data && data.allUsers && data.allUsers.nodes) {
-        // setUsers(data.allUsers.nodes)
+        setUsers(data.allUsers.nodes as User[])
       }
     }
     if (error) {
@@ -22,11 +22,11 @@ const Users: React.FC = () => {
   return (
     <div>
       <h2>Hello users,</h2>
-      {/*users &&
+      {users &&
         users.length > 0 &&
         users.map((user: User, index: number) => (
           <p key={`user_${index}`}>{user.name}</p>
-        ))*/}
+        ))}
     </div>
   );
 };
