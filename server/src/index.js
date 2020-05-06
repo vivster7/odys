abfb42120fff9dc3ebf81360338d5a84b8576224
Legarlin <legarlin@welkinhealth.com>
2020-05-06 16:34:03 -0700

    revert postgraphile

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
 const bodyParser = require('body-parser')
-const postgraphile = require('./postgraphile')
 const app = express()
 
 const { PORT } = process.env
@@ -10,6 +9,5 @@ const { PORT } = process.env
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(postgraphile)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
