b0c2190bbd73bbbd8a455e3b612f7ca46ab41b74
Legarlin <legarlin@welkinhealth.com>
2020-05-18 17:52:52 -0700

    remove step to copy postgrest.production.config and set env vars in console.cloud.google.com

--- postgrest/deploy/start.sh
+++ postgrest/deploy/start.sh
@@ -3,5 +3,5 @@
 # This will start the postgrest server + put the caddy reverse proxy in front of it.
 # It is tightly coupled with the Dockerfile.
 
-postgrest postgrest.production.conf&
+postgrest /etc/postgrest.conf&
 caddy run --config caddy.json
