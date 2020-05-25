59eb6d770b593ac28dacc1e31e635e724388debd
Vivek Dasari <vivdasari@gmail.com>
2020-05-24 19:59:51 -0700

    rename DrawingBoard -> Board

--- client/src/modules/room/Room.tsx
+++ client/src/modules/room/Room.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import DrawingBoard from '../board/DrawingBoard';
+import DrawingBoard from '../board/Board';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getOrCreateRoom } from './room.reducer';
